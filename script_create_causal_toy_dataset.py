8939ae281df8623fee969145b242cdf2e54493a2
Matej Zecevic <email@matej-z.de>
2020-12-11 18:46:24 +0100

    Created new Discretized Dataset

--- script_create_causal_toy_dataset.py
+++ script_create_causal_toy_dataset.py
@@ -18,12 +18,16 @@ Structural Causal Model
 
 class SCM_Health():
 
-    def __init__(self):
+    def __init__(self, discrete=True):
 
         age = lambda size: np.random.randint(low=18,high=80+1,size=size)
         food_habit = lambda age: 1 if age >= 40 else np.random.binomial(1,0.5)
-        health = lambda age, food_habit: 3/6 * (1 - (age-18)/(80-18)) + 2/6 * food_habit + 1/6 * np.random.binomial(1,0.5)
-        mobility = lambda health: 4/5 * health + 1/5 * np.random.binomial(1,0.5)
+        if discrete:
+            health = lambda age, food_habit: (0 if age >= 60 else (1.5 if age >= 30 else 3)) + food_habit + np.random.binomial(1,0.5)
+            mobility = lambda health: 1/2 * health + np.random.binomial(1,0.5)
+        else:
+            health = lambda age, food_habit: 3/6 * (1 - (age-18)/(80-18)) + 2/6 * food_habit + 1/6 * np.random.binomial(1,0.5)
+            mobility = lambda health: 4/5 * health + 1/5 * np.random.binomial(1,0.5)
 
         self.equations = {
             'A': age,
@@ -44,13 +48,13 @@ class SCM_Health():
 # create a dataset
 np.random.seed(0)
 N = 1000
-scm = SCM_Health()
+scm = SCM_Health(discrete=False)
 data = scm.create_data_sample(N)
 print('First 10 samples from a total of {} samples:\n'
       '\tAge         = {}\n'
       '\tFood Habits = {}\n'
       '\tHealth      = {}\n'
-      '\tMobility    = {}'.format(N, data['A'], data['F'], data['H'], data['M']))
+      '\tMobility    = {}'.format(N, data['A'][:10], data['F'][:10], data['H'][:10], data['M'][:10]))
 
 # plot a heatmap of Health in sample w.r.t. Age and Food Habits (direct causal parents)
 import matplotlib.pyplot as plt
@@ -86,3 +90,52 @@ axes = plt.gca()
 axes.set_xlim([18-1,80+1])
 axes.set_ylim([0,1])
 plt.show()
+
+
+'''
+discrete Data
+'''
+# create a dataset
+np.random.seed(0)
+N = 1000
+scm = SCM_Health(discrete=True)
+data = scm.create_data_sample(N)
+print('(Discrete) First 10 samples from a total of {} samples:\n'
+      '\tAge         = {}\n'
+      '\tFood Habits = {}\n'
+      '\tHealth      = {}\n'
+      '\tMobility    = {}'.format(N, data['A'][:10], data['F'][:10], data['H'][:10], data['M'][:10]))
+
+# plot the median health per age group
+median_health_per_age = []
+mean_health_per_age = []
+std_health_per_age = []
+for a in range(18,80+1):
+    indices = np.where(data['A'] == a)[0]
+    corresponding_health_data = [data['H'][ind] for ind in indices]
+    median_health = np.median(corresponding_health_data)
+    mean_health = np.mean(corresponding_health_data)
+    std_health = np.std(corresponding_health_data)
+    median_health_per_age.append(median_health)
+    mean_health_per_age.append(mean_health)
+    std_health_per_age.append(std_health)
+
+plt.figure(figsize=(12,6))
+plt.plot(range(18,80+1), median_health_per_age, label='Median', color='b')
+plt.plot(range(18,80+1), mean_health_per_age, label='Mean', color='green')
+plt.errorbar(range(18,80+1), mean_health_per_age, yerr=std_health_per_age, color='green')
+plt.title('Health $H$ per Age group $A$ (Sampled {} Persons via SCM)'.format(N))
+plt.xlabel('Age $A$')
+plt.ylabel('Health $H$')
+plt.legend()
+axes = plt.gca()
+axes.set_xlim([18-1,80+1])
+axes.set_ylim([0,5])
+plt.show()
+
+save=False
+
+if save:
+    import pickle
+    with open('causal_health_toy_data_discrete.pkl', 'wb') as f:
+        pickle.dump(data, f)
