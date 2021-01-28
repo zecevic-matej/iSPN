6c19840b71086f608e5e4feba7a4af62298cfc85
Matej Zecevic <email@matej-z.de>
2021-01-28 18:48:28 +0100

    Collecting Benchmark Data for Training: bnlearn; Added ASIA dataset

--- datasets/other/collect_benchmark_bnlearn_data.py
+++ datasets/other/collect_benchmark_bnlearn_data.py
@@ -1,13 +1,25 @@
 import bnlearn as bn
 import numpy as np
+import os
+import pickle
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
+
+# general parameters
+bif_file = 'asia.bif'
+visualize_immediate = False
+visualize_final = True
+N = 10000
+interventions = ['tub','lung','either','dysp']
+save_dir = 'benchmark_data_for_uniform_interventions/'
 
 # load model
-bif_file = 'asia.bif'
 model_obs = bn.import_DAG(bif_file)
+model_obs.update({'interv': None})
 
-# visualize graph
-visualize = False
-if visualize:
+# visualize_immediate graph
+if visualize_immediate:
     bn.plot(model_obs)
 
 # plot CPTs
@@ -25,11 +37,11 @@ def print_cpds(model):
 print_cpds(model_obs)
 
 # generate samples
-N = 10000
 df_obs = bn.sampling(model_obs, n=N)
+model_obs.update({'data': df_obs})
 
 # example intervention on tub
-def intervene(model, var):
+def intervene(model, var, intervention='uniform'):
     cpts = model['model'].get_cpds()
     modeled_vars = [c.variable for c in cpts]
     assert(len(modeled_vars) == len(cpts))
@@ -37,14 +49,41 @@ def intervene(model, var):
     sel_cpt = cpts[modeled_vars.index(var)]
     conditions = sel_cpt.variables.copy()
     conditions.remove(var)
-    # remove parents
-    for v in conditions:
-        model['model'].remove_node(v)
     # overwrite cpt with intervention cpt
-    sel_cpt.values = np.array([0., 1.])
+    if intervention == 'uniform':
+        sel_cpt.values = np.array([0.5, 0.5])
+    elif intervention == 'one':
+        sel_cpt.values = np.array([0., 1.])
+    else:
+        raise Exception('No Intervention possible.')
+    # remove parents edges (just within cpt)
+    sel_cpt.variables = [var]
+    sel_cpt.cardinality = [len(sel_cpt.values),1]
+    model['model'] = model['model'].do(var)
+    model['adjmat'][var] = False # columns in adjmat are targets i.e., in-going arrows, which are deleted
     print('\nIntervened on {}'.format(var))
     print_cpds(model)
-model_int = model_obs.copy()
-intervene(model_int, 'tub')
-df_int = bn.sampling(model_int, n=N)
-assert(sum(df_obs['tub']) < sum(df_int['tub']))
+
+models = [model_obs]
+for interv in interventions:
+    models.append({'model': model_obs['model'].copy(), 'adjmat': model_obs['adjmat'].copy(), 'interv': interv})
+    intervene(models[-1], interv, intervention='uniform')
+    if visualize_immediate:
+        bn.plot(models[-1])
+    models[-1].update({'data': bn.sampling(models[-1], n=N)})
+
+if visualize_final:
+    fig, axs = plt.subplots(3, 2, sharex=True, sharey=True, figsize=(10,8))
+    for i, m in enumerate(models):
+        sns.heatmap(models[i]['adjmat'], vmin=0, vmax=1, ax=axs.flatten()[i])
+        axs.flatten()[i].set_title('Intervention on {}'.format(models[i]['interv']))
+    plt.suptitle('Graphs for {}'.format(bif_file))
+    plt.show()
+
+if save_dir:
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    save_loc = os.path.join(save_dir, '{}_uniform_interventions_N{}.pkl'.format(bif_file.split('.bif')[0], N))
+    with open(save_loc,'wb') as f:
+        pickle.dump(models, f)
+    print('Saved @ {}'.format(save_loc))
